73f09d72b345dcac1bbbeb14bae75d05b74b9efb
GurudattShivani <gurudatt99s@gmail.com>
2018-11-04 18:39:30 +1100

    Add files via upload

--- CompanyDetails.Test/TestCompanyDBTest.cs
+++ CompanyDetails.Test/TestCompanyDBTest.cs
@@ -9,28 +9,85 @@ using System.Collections.Generic;
 namespace CompanyDetails.Test
 {
     [TestClass]
-    public class TestCompanyDBTest
-    {
+    public class TestCompanyDBTest    {        
+
+        [TestMethod]
+        public void TestGetCompanies()
+        {
+            var mockSet = GenerateMock();
+            var mockContext = new Mock<CompanyDBEntities>();
+            mockContext.Setup(m => m.Companies).Returns(mockSet.Object);
+            var companyContext = new CompanyDBContext(mockContext.Object);
+
+            var comp = companyContext.GetAllCompanies().ToList();            
+
+            Assert.AreEqual(3, comp.Count);
+            Assert.AreEqual(1, comp[0].CompanyId);
+            Assert.AreEqual(2, comp[1].CompanyId);
+            Assert.AreEqual(3, comp[2].CompanyId);
+        }
+
+        [TestMethod]
+        public void TestIfCompanyExistById()
+        {
+            var mockSet = GenerateMock();
+            var mockContext = new Mock<CompanyDBEntities>();
+            mockContext.Setup(m => m.Companies).Returns(mockSet.Object);
+            var companyContext = new CompanyDBContext(mockContext.Object);
+
+            var comp = companyContext.GetCompanyById(1);
+            var findComp = companyContext.FindCompanyById(1);
+
+            Assert.AreEqual(1, findComp.CompanyId);
+            Assert.AreEqual(1, comp.Count);
+            Assert.AreEqual(1, comp[0].CompanyId);
+        }
+
         [TestMethod]
         public void TestIfCompanyExistOnAdd()
         {
-            var mockSet = new Mock<DbSet<Company>>();
-            
+            var mockSet = new Mock<DbSet<Company>>();            
             var mockContext = new Mock<CompanyDBEntities>();
             mockContext.Setup(m => m.Companies).Returns(mockSet.Object);
 
-            var commpanyContext = new CompanyDBContext(mockContext.Object);
-            commpanyContext.AddCompany(new Company() { CompanyId = 1, CompanyName = "Test",
+            var companyContext = new CompanyDBContext(mockContext.Object);
+            companyContext.AddCompany(new Company() { CompanyId = 1, CompanyName = "Test",
                 Email = "test@test.com", LastName = "Testing", PhoneNumber = 123456 });
-
             mockSet.Verify(m => m.Add(It.IsAny<Company>()), Times.Once());
         }
 
         [TestMethod]
-        public void TestIfCompanyExistOnDelete()
+        public void TestIfDetailsUpdated()
+        {
+            var mockSet = GenerateMock();
+            var mockContext = new Mock<CompanyDBEntities>();
+            mockContext.Setup(m => m.Companies).Returns(mockSet.Object);
+            var companyContext = new CompanyDBContext(mockContext.Object);
+
+            //Get a company by Id
+            var firstComp = companyContext.FindCompanyById(1);            
+            firstComp.CompanyName = "Updated Company";            
+
+            //Get a list of companies
+            var comp = companyContext.GetCompanyById(1);
+            Assert.AreEqual("Updated Company", comp[0].CompanyName); //Updated data           
+        }
+
+        [TestMethod]
+        public void TestOnDelete()
+        {
+            var mockSet = GenerateMock();
+            var mockContext = new Mock<CompanyDBEntities>();
+            mockContext.Setup(m => m.Companies).Returns(mockSet.Object);
+            var companyContext = new CompanyDBContext(mockContext.Object);
+
+            companyContext.DeleteCompany(companyContext.FindCompanyById(1));
+            mockSet.Verify(m => m.Remove(It.IsAny<Company>()), Times.Once());
+        }
+
+        public Mock<DbSet<Company>> GenerateMock()
         {
             var mockSet = new Mock<DbSet<Company>>();
-
             var data = new List<Company>
             {
                 new Company { CompanyId = 1 },
@@ -42,19 +99,7 @@ namespace CompanyDetails.Test
             mockSet.As<IQueryable<Company>>().Setup(m => m.Expression).Returns(data.Expression);
             mockSet.As<IQueryable<Company>>().Setup(m => m.ElementType).Returns(data.ElementType);
             mockSet.As<IQueryable<Company>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
-
-            var mockContext = new Mock<CompanyDBEntities>();
-            mockContext.Setup(m => m.Companies).Returns(mockSet.Object);
-            
-
-
-            var commpanyContext = new CompanyDBContext(mockContext.Object);
-            var comp = commpanyContext.GetAllCompanies().ToList();
-
-            Assert.AreEqual(3, comp.Count);
-            Assert.AreEqual(1, comp[0].CompanyId);
-            Assert.AreEqual(2, comp[1].CompanyId);
-            Assert.AreEqual(3, comp[2].CompanyId);
+            return mockSet;            
         }
-    }
+    }    
 }
